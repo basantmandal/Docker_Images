d3c936e9381d840ece36b59da847463573b52ec8
Basant Mandal <basantmandal@gmail.com>
2023-07-15 11:28:55 +0530

    Added PHP7.3 Docker Image Support

---
+++ HK2_Php7.3-Fpm/build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+image="basantmandal/hk2-php7.3-fpm"
+version="1.0"
+
+result=$(docker images -q ${image})
+if [[ -n "$result" ]]; then
+    echo "Image exists"
+    sudo docker image rm -f ${result}
+else
+    docker build --no-cache -t ${image}:${version} . >> docker_build.log
+    docker tag ${image}:${version} ${image}:${version}
+fi
